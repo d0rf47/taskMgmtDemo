027b145dbc4be4986b9540b23a4f0fd009e21505
d0rf47 <48887571+d0rf47@users.noreply.github.com>
2019-10-28 11:34:37 -0400

    origin

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose')
 const dbURL = 'mongodb+srv://Admin2:xuZNKjL2aHsqrlpv@falconbnb-ppoyi.gcp.mongodb.net/test?retryWrites=true&w=majority';
-const router = express.Router();
-
+const taskRouter =  require('./routes/Task')
+const userRouter =  require('./routes/User')
+const mainRouter =  require('./routes/General')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.use('/', mainRouter);
+app.use("/user", userRouter);
+app.use('/task', taskRouter);
 
 mongoose.connect(dbURL,{ useUnifiedTopology: true, useNewUrlParser: true })
     .then(()=>{
@@ -20,7 +24,6 @@ mongoose.connect(dbURL,{ useUnifiedTopology: true, useNewUrlParser: true })
     .catch(err=>{
         console.log(`Connection Error: ${err}`);
 })
-
 
 
 
